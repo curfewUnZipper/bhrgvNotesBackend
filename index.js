7a80cfc6674661b8e328af61022f2755e1a90026
Da Evil <123114180+curfewUnZipper@users.noreply.github.com>
2024-08-15 18:05:42 +0530

    added get / again

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
+app.get("/", (req,res)->{
+  res.send("HELLO THERE!");
+}
+
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
