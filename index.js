044ff27908334829d70c27676506312d4fb0ad0d
Da Evil <cryingpushpa.com>
2024-08-15 16:45:16 +0530

    updated PORT variable

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 connectToMongo();
 
 const app = express();
-const port = process.env.PORT2 || 5000;
+const port = process.env.PORT || 5000;
 
 const http = require("http");
 
